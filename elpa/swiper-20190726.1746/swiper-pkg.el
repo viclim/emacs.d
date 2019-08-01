9bbca6e65a5babe454f27593f284c5f0f14d69d0
Victor Lim <victor@rrsoft.co>
2019-08-01 14:37:50 +0800

    packages update

---
+++ elpa/swiper-20190726.1746/swiper-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "swiper" "20190726.1746" "Isearch with an overview. Oh, man!" '((emacs "24.1") (ivy "0.12.0")) :commit "e4e0ec45b7bc77d40f7c2f89fb8b131eb7681e82" :keywords '("matching") :authors '(("Oleh Krehel" . "ohwoeowho@gmail.com")) :maintainer '("Oleh Krehel" . "ohwoeowho@gmail.com") :url "https://github.com/abo-abo/swiper")
